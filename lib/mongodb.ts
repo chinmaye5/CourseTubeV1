cdaa1a00fbee0893f23ea4fdf30269203b4d588c
Chinmaye HG <154550034+chinmaye5@users.noreply.github.com>
2025-11-27 14:07:41 +0530

    Use environment variable for MongoDB URI

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://chinmaye:chinmaye@cluster0.ii1sy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
